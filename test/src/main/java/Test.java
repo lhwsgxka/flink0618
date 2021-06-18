540a7bc258ef04d0294dceff722f8c973b5a9a49
lhwsgxka <lhwsgxka@163.com>
2021-06-18 13:23:25 +0800

    这是一哥简单的测试

---
+++ test/src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author lhw
+ * @version 1.0
+ * @date 2021/6/18 13:22
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是一个简单的测试");
+
+    }
+}
